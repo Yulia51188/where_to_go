8baa6ec1490233df69693d6d4fdc21c1585a6fa0
Yulia51188 <sun51188@gmail.com>
2021-12-10 13:17:17 +0300

    Add photo preview to inline section in admin

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from places.models import Place, PlacePhoto
+from django.utils.safestring import mark_safe
 
 
 class PlacePhotoInline(admin.TabularInline):
     model = PlacePhoto
+    readonly_fields = ['image_preview']
+    fields = ('image', 'image_preview', 'index')
+
+    def image_preview(self, obj, max_height=200):
+        preview_template = '<img src="{url}" height="{height}" width="{width}"/>'
+        width = obj.image.width * max_height / obj.image.height
+        return mark_safe(preview_template.format(
+            url=obj.image.url,
+            height=max_height,
+            width=width,
+        ))
 
 
 class PlaceAdmin(admin.ModelAdmin):
