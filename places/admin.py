1e8b339ffcb410cd48348ea8913fdc26563c73aa
Yulia51188 <sun51188@gmail.com>
2021-12-22 14:46:59 +0300

    Format html with mark_safe and format_html utils

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from places.models import Place, PlacePhoto
 from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 from adminsortable2.admin import SortableInlineAdminMixin
 
 
@@ -12,13 +13,14 @@ class PlacePhotoInline(SortableInlineAdminMixin, admin.TabularInline):
     fields = ('image', 'image_preview', 'index')
 
     def image_preview(self, obj, max_height=200):
-        preview_template = '<img src="{url}" height="{height}" width="{width}"/>'
+        preview_template = '<img src="{}" height="{}" width="{}"/>'
         width = obj.image.width * max_height / obj.image.height
-        return mark_safe(preview_template.format(
-            url=obj.image.url,
-            height=max_height,
-            width=width,
-        ))
+        return format_html(
+            preview_template,
+            mark_safe(obj.image.url),
+            mark_safe(max_height),
+            mark_safe(width),
+        )
 
 
 class PlaceAdmin(admin.ModelAdmin):
